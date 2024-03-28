983b338c2f9fdfd10de7ac805a511dfffa9963f3
lasernoy <cloudy.nny07@gmail.com>
2024-03-28 13:28:39 +0800

    html

---
+++ src/Calculator.jsx
@@ -0,0 +1,22 @@
+
+
+
+/** JAVASCRIPT CALCULATOR APP
+    * 
+    */
+class Calculator extends React.Component => {
+
+  constructor (props) {
+    super (props) {};
+  };
+
+  render () {
+    return ();
+  };
+};
+
+
+// export
+export default Calculator;
+
+
